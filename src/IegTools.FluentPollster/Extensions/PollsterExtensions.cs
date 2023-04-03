207659867f5bfc379eff385baaf7190dfa72a5e1
Ernst <ernst.greiner@ieg-tools.net>
2023-04-03 14:21:27 +0200

    prepare for NuGet packaging 1/

---
+++ src/IegTools.FluentPollster/Extensions/PollsterExtensions.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable once CheckNamespace
+namespace IegTools.FluentPollster;
+
+public static class PollsterExtensions
+{
+    /// <summary>
+    /// Returns true if the minute and optional minute-offset of the specified date-time
+    /// is divisible by everyMinutes with remainder is 0
+    /// </summary>
+    /// <param name="time">The date-time</param>
+    /// <param name="everyMinutes">Every x minutes</param>
+    /// <param name="offsetMinute">The offset minute</param>
+    public static bool IsCurrentMinuteDivisibleBy(this DateTime time, int everyMinutes, int offsetMinute = 0) =>
+        (time.Minute + offsetMinute) % everyMinutes == 0;
+}
