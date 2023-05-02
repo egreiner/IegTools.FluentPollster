f28860925e9eaf0c53bc1a3fc36f1c94e9e1bd2f
Ernst <ernst.greiner@ieg-tools.net>
2023-05-02 12:41:51 +0200

    add IsDivisibleBySeconds()

--- src/IegTools.FluentPollster/Extensions/DateTimeExtensions.cs
+++ src/IegTools.FluentPollster/Extensions/DateTimeExtensions.cs
@@ -15,4 +15,18 @@ public static class DateTimeExtensions
     /// <param name="offsetMinute">The offset minute</param>
     public static bool IsMinuteDivisibleBy(this DateTime time, int everyMinutes, int offsetMinute = 0) => 
         everyMinutes > 0 && (time.Minute - offsetMinute) % everyMinutes == 0;
+    
+    /// <summary>
+    /// Returns true if the combined seconds
+    /// (derived from the minute and second values of the specified date-time, adjusted by an optional seconds-offset)
+    /// are divisible by the provided <see cref="secondsDivisor"/> (with a remainder of 0) within a span of 10 seconds.
+    /// </summary>
+    /// <param name="time">The DateTime</param>
+    /// <param name="secondsDivisor">The second divisor</param>
+    /// <param name="offsetSeconds">The offset minute</param>
+    public static bool IsDivisibleBySeconds(this DateTime time, int secondsDivisor, int offsetSeconds = 0)
+    {
+        var seconds = (time.Minute * 60 + time.Second - offsetSeconds) / 10;
+        return seconds % (secondsDivisor / 10) == 0;
+    }
 }
--- tests/IntegrationTests.FluentPollster/Extensions/PollsterExtensionsTests.cs
+++ tests/IntegrationTests.FluentPollster/Extensions/PollsterExtensionsTests.cs
@@ -49,4 +49,52 @@ public class PollsterExtensionsTests
 
         actual.Should().Be(expected);
     }
+    
+    [Theory]
+    [InlineData(00, 00, 00, 150, true)]
+    [InlineData(00, 00, 09, 150, true)]
+    [InlineData(00, 00, 10, 150, false)]
+    [InlineData(00, 01, 10, 150, false)]
+    [InlineData(01, 01, 10, 150, false)]
+    [InlineData(01, 02, 20, 150, false)]
+    [InlineData(01, 02, 30, 150, true)]
+    [InlineData(01, 02, 40, 150, false)]
+    [InlineData(01, 05, 05, 150, true)]
+    [InlineData(12, 07, 39, 150, true)]
+    [InlineData(00, 00, 00, 090, true)]
+    [InlineData(00, 01, 30, 090, true)]
+    [InlineData(00, 01, 39, 090, true)]
+    [InlineData(00, 01, 40, 090, false)]
+    public void Test_IsDivisibleBySeconds(int hour, int minute, int second, int divisor, bool expected)
+    {
+        var time = new DateTime(2023,5, 2, hour, minute, second);
+
+        var actual = time.IsDivisibleBySeconds(divisor);
+
+        actual.Should().Be(expected);
+    }
+    
+    [Theory]
+    [InlineData(00, 00, 10, 150, true)]
+    [InlineData(00, 00, 19, 150, true)]
+    [InlineData(00, 00, 20, 150, false)]
+    [InlineData(00, 01, 20, 150, false)]
+    [InlineData(01, 01, 20, 150, false)]
+    [InlineData(01, 02, 30, 150, false)]
+    [InlineData(01, 02, 40, 150, true)]
+    [InlineData(01, 02, 50, 150, false)]
+    [InlineData(01, 05, 15, 150, true)]
+    [InlineData(12, 07, 49, 150, true)]
+    [InlineData(00, 00, 10, 090, true)]
+    [InlineData(00, 01, 40, 090, true)]
+    [InlineData(00, 01, 49, 090, true)]
+    [InlineData(00, 01, 50, 090, false)]
+    public void Test_IsDivisibleBySeconds_with_SecondsOffset(int hour, int minute, int second, int divisor, bool expected)
+    {
+        var time = new DateTime(2023,5, 2, hour, minute, second);
+
+        var actual = time.IsDivisibleBySeconds(divisor, 10);
+
+        actual.Should().Be(expected);
+    }
 }
